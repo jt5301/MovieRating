bbaf7ea71f5aed2ae71ed7224493266bcb68af67
John Tieu <johnson.tieu@gmail.com>
2020-11-25 15:44:35 -0500

    added in template, context

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,71 +1,19 @@
-import React, { Component } from "react";
+import React from 'react';
+import NavbarSearch from './NavbarSearch.js'
+import MovieDisplay from './MovieDisplay.js'
+import { SearchProvider } from '../hooks/SearchContext.js'
 
-import { Typography } from "@material-ui/core";
-import { withStyles } from "@material-ui/core/styles";
-import { Route, Link } from "react-router-dom";
 
-import Ping from "./Ping";
+export default function Landing() {
 
-const landinPageStyle = theme => ({
-  landingContainer: {
-    margin: theme.spacing.unit * 2
-  }
-});
 
-class LandingPage extends Component {
-  state = {
-    welcomeMessage: "Step 1: Run the server and refresh (not running)",
-    step: 0
-  };
+  return (
+    <React.Fragment>
+      <SearchProvider>
+        <NavbarSearch />
+        <MovieDisplay />
+      </SearchProvider>
 
-  componentDidMount() {
-    fetch("/welcome")
-      .then(res => {
-        console.log(res);
-        if (res.status === 200) return res.json();
-        else throw Error("Couldn't connect to the server");
-      })
-      .then(res => {
-        this.setState({ welcomeMessage: res.welcomeMessage });
-        this.incrementStep();
-      })
-      .catch(err => {
-        console.log(err.message);
-      });
-  }
-
-  incrementStep = () => {
-    this.setState(prevState => ({ step: (prevState.step += 1) }));
-  };
-
-  render() {
-    const { classes } = this.props;
-    return (
-      <div className={classes.landingContainer}>
-        <Typography>{this.state.welcomeMessage}</Typography>
-        {this.state.step >= 1 && (
-          <React.Fragment>
-            <Link to="/ping">Step 2: Click here </Link>
-            <Route
-              path="/ping"
-              render={props => {
-                return (
-                  <Ping
-                    {...props}
-                    incrementStep={this.incrementStep}
-                    step={this.state.step}
-                  />
-                );
-              }}
-            />
-          </React.Fragment>
-        )}
-        {this.state.step >= 3 && (
-          <Typography>All done! Now go make a pull request!</Typography>
-        )}
-      </div>
-    );
-  }
+    </React.Fragment>
+  );
 }
-
-export default withStyles(landinPageStyle)(LandingPage);
